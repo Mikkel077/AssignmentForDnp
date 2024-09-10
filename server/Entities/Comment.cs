a865520859db1cbf759668ed7d5644fe6c40ff75
mikkel077 <mikkelhansen077@gmail.com>
2024-09-10 22:24:48 +0200

    First repo

--- server/Entities/Comment.cs
+++ server/Entities/Comment.cs
@@ -4,6 +4,6 @@ public class Comment
 {
     public int ID { get; set; }
     public String Body { get; set; }
-    
-    
+    public int PostID { get; set; }
+    public int UserID { get; set; }
 }
