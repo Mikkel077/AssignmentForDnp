3c672e8ff7c68049806fedbd10f99152df16715a
mikkel077 <mikkelhansen077@gmail.com>
2024-09-10 13:51:39 +0200

    Entities added

---
+++ server/Entities/Comment.cs
@@ -0,0 +1,9 @@
+namespace Entities;
+
+public class Comment
+{
+    public int ID { get; set; }
+    public String Body { get; set; }
+    
+    
+}
---
+++ server/Entities/User.cs
@@ -0,0 +1,8 @@
+namespace Entities;
+
+public class User
+{
+    public int ID { get; set; }
+    public String Username { get; set; }
+    public String Password { get; set; }
+}
